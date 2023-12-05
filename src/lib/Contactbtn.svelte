3917bc5ceaf187fc81cb8680ccdef0e34c49803b
Sydney Bruce <sydneybruce20@gmail.com>
2023-12-05 08:27:57 -0700

    pencil animation

--- src/lib/Contactbtn.svelte
+++ src/lib/Contactbtn.svelte
@@ -1,3 +1,9 @@
 <!-- TODO change to contact sheet -->
 <a href="/" class="max-w-max"><button class="bg-[#F6C3AE] max-w-max px-6 py-2 text-lg mx-4 font-semibold">contact</button></a>
 
+<style>
+    button {
+    -webkit-box-shadow: 3px 3px 0px 2px #000000; 
+    box-shadow: 3px 3px 0px 2px #000000;
+  }
+</style>
--- src/lib/ReverseWorkcard.svelte
+++ src/lib/ReverseWorkcard.svelte
@@ -1,11 +1,16 @@
+<script>
+  export let revImg = "/placeholder.jpg";
+  export let revAlt = "placeholder";
+  export let revProjectTitle = "Title of Project"
+  export let revDescription = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus hic quisquam iste ipsum earum tenetur deserunt, nam impedit officiis? Assumenda amet excepturi libero cumque ab totam dignissimos esse? Odio, excepturi? Ratione, debitis. Debitis quam soluta."
+</script>
+
 <aside id="card-work" class="min-w-screen grid grid-cols-12 grid-rows-2">
   <div class="border-[1.5px] bg-white border-black w-[20rem] flex flex-col z-10 col-start-9 col-span-3 row-start-1 row-span-2 self-center">
-    <h5 class="text-2xl border-b-[1px] border-black m-4 p-2 mt-[2rem]">Title of Project</h5>
-    <p class="m-4 text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus hic quisquam iste ipsum earum tenetur deserunt, nam impedit officiis? Assumenda amet excepturi libero cumque ab totam dignissimos esse? Odio, excepturi?
-    Ratione, debitis. Debitis quam soluta.
-    </p>
+    <h5 class="text-2xl border-b-[1px] border-black m-4 p-2 mt-[2rem]">{revProjectTitle}</h5>
+    <p class="m-4 text-lg"> {revDescription}</p>
   </div>
   <div class="max-w-max border-[1.5px] border-black col-span-9 col-start-2 row-start-1">
-  <img src="/placeholder.jpg" alt="placeholder" class="w-[1050px] h-[400px]">
+  <img src={revImg} alt={revAlt} class="w-[1050px] h-[400px]">
   </div>
 </aside>
