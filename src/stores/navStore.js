746bd0e081ae71bc73241238601e37dc72f51df0
Sydney Bruce <sydney@symbioticai.ca>
2024-05-25 11:01:21 -0600

    changed default nav links

--- src/stores/navStore.js
+++ src/stores/navStore.js
@@ -1,8 +1,8 @@
 import { writable } from 'svelte/store';
 
 export const navItems = writable([
-  { name: 'About', href: '#about' },
-  { name: 'Projects', href: '#projects' },
+  { name: 'About', href: '/' },
+  { name: 'Projects', href: '/' },
 ]);
   // export let navItems = [
   //   {name: "About", href:"#about"},
