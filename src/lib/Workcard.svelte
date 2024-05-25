b8a0e8e53a227903b9780ac841d43442628bcac3
Sydney Bruce <sydney@symbioticai.ca>
2024-05-25 11:53:01 -0600

    added accent bg hover

--- src/lib/Workcard.svelte
+++ src/lib/Workcard.svelte
@@ -7,11 +7,10 @@
 </script>
 
 <aside id="card-work" class="min-w-screen grid grid-cols-12 grid-rows-2"> 
-  <!-- TODO: ADD PROJECT -->
   <div class="border-[1.5px] bg-white border-black w-[16rem] lg:w-[20rem] flex flex-col z-10 col-start-2 col-span-4 row-start-1 row-span-2 self-center">
     <h5 class="text-2xl border-b-[1px] border-black m-4 p-2 mt-[2rem]">{projectTitle}</h5>
     <p class="mt-4 mx-4 text-lg">{description}</p>
-    <a href={href}><button class="border-[1px] border-black self-start min-w-[40%] mx-4 px-4 py-2 my-4 mb-6 hover:bg-pupink text-lg font-semibold drop-shadow-flat bg-pupink hover:bg-[#F0743E]">See More</button></a>
+    <a href={href}><button class="border-[1px] border-black self-start min-w-[40%] mx-4 px-4 py-2 my-4 mb-6 text-lg font-semibold drop-shadow-flat bg-pupink hover:bg-accent">See More</button></a>
   </div>
   <div class="max-w-max border-[1.5px] border-black col-span-9 col-start-3 row-start-1">
   <img src={img} alt={alt} class="w-[1050px] h-[400px] bg-cover">
