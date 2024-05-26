9e98b2e9bc25583694859c7f8d8769d9176748bf
Sydney Bruce <sydney@symbioticai.ca>
2024-05-26 15:03:35 -0600

    fixed contact form issue on mobile

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -50,15 +50,15 @@ updateNavItems(customNavItems);
 
 <body class=" flex min-h-screen w-screen m-0 overflow-x-hidden">
   <!-- this is the animation -->
-  <div class="lg:ml-[8rem] self-start my-[2rem] mr-[2rem]">
+  <div class="lg:ml-[8rem] self-start my-[2rem] mr-[2rem] w-0 lg:w-auto">
     <div class=" bg-[url('/assets/sky.png')] lg:w-[450px] lg:h-[550px] m-4 md:w-[350px] md:h-[450px] h-0 w-0 flex items-center justify-center ">
       <img id="wings" src="/assets/wings.svg" alt="wings" class="w-auto md:h-auto w-0 h-0 px-2">
       <img id="mail" src="/assets/mail.svg"alt="mail" class="md:w-[230px] lg:w-auto absolute w-0">
     </div>
   </div>
 <!-- form section -->
-  <section class="self-start flex flex-col overflow-x-hidden ml-0 mt-6 w-[40%]">
-    <h3 class=" text-3xl lg:text-6xl mb-6 mr-6 ml-6 lg:my-[1.7rem]">Contact Me</h3>
+  <section class="self-start flex flex-col overflow-x-hidden ml-0 mt-6 w-[80%] lg:w-[40%]">
+    <h3 class=" text-4xl lg:text-6xl mb-6 mr-6 ml-6 lg:my-[1.7rem]">Contact Me</h3>
     <div class="flex flex-col border-[1.5px] border-black bg-white mx-6  justify-self-end mt-2">
       <!-- form -->
     <form id="myform" name="contact" method="POST" data-netlify="true" class="flex flex-col align-items-start p-4 mx-4 justify-self-end  ">
